462eee877c8b2e30eb7377165985aeaf2965fe76
Will <will_munns@iname.com>
2021-09-18 19:51:25 +0100

    Changes for RoboCon 2022 - update for Python3

--- helper.py
+++ helper.py
@@ -1,22 +1,28 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+
+# This file may be obsolete - need to check if scaling in shephard is laggy
 
 import os
 import time
 
 from PIL import Image
 
+
+static_path = 'shepherd/static/'
+input_file = static_path + "full_image.jpg"
+output_file = static_path + "image.jpg"
+stat_file = static_path + "imgtime.txt"
 mod_time = 0
 out_width = 640.0
 out_height = 480.0
 
 while True:
     time.sleep(0.1)
-    if os.path.exists("/tmp/colimage.jpg"):
-        stat_obj = os.stat("/tmp/colimage.jpg")
+    if os.path.exists(input_file):
+        stat_obj = os.stat(input_file)
         if mod_time != stat_obj.st_mtime:
-            print("New image?")
             try:
-                img = Image.open("/tmp/colimage.jpg")
+                img = Image.open(input_file)
                 width_i, height_i = img.size
                 width = width_i * 1.0
                 height = height_i * 1.0
@@ -28,9 +34,8 @@ while True:
                     img = img.resize((int(width*scale_x), int(height*scale_x)), Image.ANTIALIAS)
 
                 mod_time = stat_obj.st_mtime
-                img.save("/root/shepherd2/shepherd/static/image.jpg","JPEG")
-                with open("/root/shepherd2/shepherd/static/imgtime.txt", "w") as myfile:
+                img.save(output_file,"JPEG")
+                with open(stat_file, "w") as myfile:
                     myfile.write(str(mod_time))
             except IOError:
                 pass
-            mod_time = stat_obj.st_mtime
--- shepherd/__init__.py
+++ shepherd/__init__.py
@@ -1,28 +1,31 @@
 # encoding: utf-8
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+
 
 import errno
 import os
+from pathlib import Path
 
 from flask import Flask, render_template, send_file
 from flask_sockets import Sockets
 from flask_cors import CORS
 import RPi.GPIO as GPIO
+from logging.handlers import SysLogHandler
+import logging
 
 from shepherd.blueprints import upload, run, pyls, editor, staticroutes
 
 START_BUTTON_PIN = 5  # This is a BCM pin number (BCM0 corresponds to phys27).
 
+syslogger = logging.getLogger()
+syslogger.addHandler(SysLogHandler('/dev/log'))
 
 app = Flask(__name__, template_folder="templates")
 sockets = Sockets(app)
 
+# Allow resources to be fetched from cross origin
 CORS(app, resources=r'/*')
-
-
 app.secret_key = os.urandom(32)
-
 
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 app.config["MAX_CONTENT_LENGTH"] = 64 * 1024 * 1024  # 64 MiB
@@ -44,13 +47,49 @@ if (not app.debug) or os.environ.get("WERKZEUG_RUN_MAIN"):
     run.init(app)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(START_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # Teamname should be set on a per brain basis before shipping
+    # Its purpose is to allow the setting of specific graphics for help identifing teams in the arena.
+    # Graphics are loaded from the ArenaUSB stick if available, or standard graphics from the stick are used.
+    # this used to be in rc.local, but the looks of shame and dissapointment got the better of me 
+
+    game_control_path = Path('/media/ArenaUSB')
+
+    teamname_file = Path('/home/pi/teamname.txt')
+    if teamname_file.exists():
+        teamname_jpg = teamname_file.read_text().replace('\n', '') +'.jpg'
+    else:
+        teamname_jpg = 'none'
+
+    # Pick a start imapge in order of preference : 
+    #     1) We have a team corner image on the USB
+    #     2) The team have uploaded their own image to the robot
+    #     3) We have a generic corner image on the USB
+    #     4) The game image
+    start_graphic = game_control_path / teamname_jpg
+    if not start_graphic.exists():
+        # attempt to find the team specific corner graphic from the ArenaUSB
+        start_graphic = Path('robotsrc/team_logo.jpg')
+    if not start_graphic.exists():
+        # attempt to find the default corner graphic from ArenaUSB
+        start_graphic = game_control_path / 'Corner.jpg'
+    if not start_graphic.exists():
+        # finally look for a game specific logo
+        start_graphic = Path('/home/pi/game_logo.jpg')
+    if start_graphic.exists():
+        # if ANY of the above paths generate a useful image, copy it into the web "static" files like an animal who doesn't understand the word static
+        # if this all fails then the user will see the last image the camera took
+        static_graphic = Path('shepherd/static/image.jpg')
+        static_graphic.write_bytes(start_graphic.read_bytes())
+
     def _start(channel):
+        # Set the zone based on files in game_contol_path, defaulting to zone 0
         zone = "0"
-        if os.path.exists("/media/ArenaUSB/zone1.txt"):
+        if (game_control_path / 'zone1.txt').exists():
             zone = "1"
-        elif os.path.exists("/media/ArenaUSB/zone2.txt"):
+        elif (game_control_path / 'zone2.txt').exists():
             zone = "2"
-        elif os.path.exists("/media/ArenaUSB/zone3.txt"):
+        elif (game_control_path / 'zone3.txt').exists():
             zone = "3"
         # this is the weirdest calling convention
         ctx = app.test_request_context(data={
