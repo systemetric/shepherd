bbab8d059e468339c2e31f8d6d84fda103ae9c77
Oli <147245351+what-does-that-do@users.noreply.github.com>
2023-10-14 21:31:15 +0100

    Tidied code

--- shepherd/websocks.py
+++ shepherd/websocks.py
@@ -2,38 +2,6 @@
 Simple code to run a websocket which sends a message out to all connected browsers
 when the image file has been changed, i.e. when an image is taken from the camera.
 '''
-
-'''
-import websockets
-import asyncio
-import os
-
-CONNECTIONS = set()
-
-async def register(websocket):#add client to list of connnected clients upon connection
-    CONNECTIONS.add(websocket)
-    print("SOMEONE CONNECTED!!")
-    try:
-        await websocket.send("Hello there person, thanks for connecting")
-        await websocket.wait_closed()
-    finally:
-        CONNECTIONS.remove(websocket)
-
-async def send_file_change_messages():#broadcasts a websocket message when image file changed
-    while True:
-        #os.system("watch -d -t -g ls /home/pi/shepherd/shepherd/static/image.jpg && echo \"changed\"")#run blocking code here waiting until file has changed
-        os.system("sleep 1")#run blocking code here waiting until file has changed
-        websockets.broadcast(CONNECTIONS, "Image updated")
-        print("Image updated message sent.")
-
-async def main():#runs the websocket
-    async with websockets.serve(register, "0.0.0.0", 5001):
-        await send_file_change_messages()
-
-if __name__ == "__main__":
-    asyncio.run(main())
-'''
-
 #!/usr/bin/env python
 
 import asyncio
